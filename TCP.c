adbb5c73c33b19b517d56e84c4e2c863269dc8c4
Naman <128227012+Naman-Kandpal@users.noreply.github.com>
2025-06-20 07:28:32 +0530

    Create TCP.c

---
+++ TCP.c
@@ -0,0 +1,184 @@
+// File: tcp_server.c
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h> // for close()
+
+#include <netinet/in.h> // for sockaddr_in
+
+#define PORT 8080
+
+#define BUFFER_SIZE 1024
+
+int main() {
+
+int server_fd, new_socket;
+
+char buffer[BUFFER_SIZE] = {0};
+
+struct sockaddr_in address;
+
+int addrlen = sizeof(address);
+
+// 1. Create socket
+
+server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+if (server_fd == 0) {
+
+perror("Socket failed");
+
+exit(EXIT_FAILURE);
+
+}
+
+// 2. Bind socket to IP/Port
+
+address.sin_family = AF_INET;
+
+address.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP
+
+address.sin_port = htons(PORT);
+
+if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+
+perror("Bind failed");
+
+exit(EXIT_FAILURE);
+
+}
+
+// 3. Listen
+
+if (listen(server_fd, 3) < 0) {
+
+perror("Listen failed");
+
+exit(EXIT_FAILURE);
+
+}
+
+printf("Server is listening on port %d...\n", PORT);
+// 4. Accept connection
+
+new_socket = accept(server_fd, (struct sockaddr*)&address,
+
+(socklen_t*)&addrlen);
+
+if (new_socket < 0) {
+
+perror("Accept failed");
+
+exit(EXIT_FAILURE);
+
+}
+
+// 5. Read message from client
+
+read(new_socket, buffer, BUFFER_SIZE);
+
+printf("Message from client: %s\n", buffer);
+
+// Optional: Send a reply
+
+char *reply = "Hello from server!";
+
+send(new_socket, reply, strlen(reply), 0);
+
+// 6. Close sockets
+
+close(new_socket);
+
+close(server_fd);
+
+return 0;
+
+}
+
+2.TCP code (C Language)
+
+// File: tcp_client.c
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h> // for close()
+
+#include <arpa/inet.h> // for sockaddr_in, inet_addr
+
+#define PORT 8080
+
+#define BUFFER_SIZE 1024
+
+int main() {
+
+int sock = 0;
+
+struct sockaddr_in serv_addr;
+
+char *message = "Hello from client!";
+
+char buffer[BUFFER_SIZE] = {0};
+
+// 1. Create socket
+
+sock = socket(AF_INET, SOCK_STREAM, 0);
+
+if (sock < 0) {
+
+printf("\n Socket creation error \n");
+
+return -1;
+
+}
+
+// 2. Set server address
+
+serv_addr.sin_family = AF_INET;
+
+serv_addr.sin_port = htons(PORT);
+
+// Convert IPv4 addresses from text to binary
+
+if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+
+printf("\nInvalid address/Address not supported \n");
+
+return -1;
+
+}
+
+// 3. Connect to server
+
+if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+printf("\nConnection Failed \n");
+return -1;
+
+}
+// 4. Send message to server
+
+send(sock, message, strlen(message), 0);
+
+printf("Message sent to server: %s\n", message);
+
+// 5. Read server reply
+
+read(sock, buffer, BUFFER_SIZE);
+
+printf("Message from server: %s\n", buffer);
+
+// 6. Close socket
+
+close(sock);
+
+return 0;
+
+}
+
